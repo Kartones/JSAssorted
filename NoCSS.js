4c0166a1b29c03fe8152f2841d7053a6ae376da2
Kartones <diego@kartones.net>
2013-06-12 20:22:50 +0200

    Bugfix + proper credits
    Original idea credits + bugfix of disabling stylesheets before removal
    (some webpages were being cleaned, others not)

--- NoCSS.js
+++ NoCSS.js
@@ -1,9 +1,14 @@
 /*
-Remove all CSS stylesheets, script blocks and includes, iframes and custom CSS styles from current page
+Remove all CSS stylesheets, script blocks and includes, iframes and custom CSS styles from current page.
+Expanded from http://dorward.me.uk/software/disablecss/ original idea
 */
 
 // add "javascript:" before the code to use it as a bookmarklet in Firefox/Chrome
-var el = document.getElementsByTagName('script');
+var el, i, size;
+for (i=0, size=document.styleSheets.length; i<size; i++) {
+    void (document.styleSheets.item(i).disabled = true);
+}
+el = document.getElementsByTagName('script');
 while (el.length > 0) {
     void (el[0].parentNode.removeChild(el[0]));
 }
