87ddac3a3e68c6c2abc63f4b43b5ddfea81b258b
Kartones <diego@kartones.net>
2014-07-19 09:25:31 +0200

    Removal of this code

--- SNCleaner.js
+++
@@ -1,9 +0,0 @@
-/*
-Careful, this will wipe out ALL your messages one by one. No confirm, no go back.
-*/
-
-// CUPCAKE / "New Tuenti" (2012/13 redesign)
-setInterval(function () {
-    YG.one("#wallpost-list").one("[type=submit]")._node.click();
-    YG.one(".act-submit.acceptAction")._node.click();
-}, 2500);
